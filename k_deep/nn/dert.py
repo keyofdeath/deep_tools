3474283c1b47454e2f265a1de14334e3a2c92d76
swan.blanc@etu.univ-smb.fr <magicswan1995>
2020-06-21 11:11:01 +0200

    Add dert basic

---
+++ k_deep/nn/dert.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+import os
+import logging.handlers
+
+import tensorflow as tf
+
+PYTHON_LOGGER = logging.getLogger(__name__)
+if not os.path.exists("log"):
+    os.mkdir("log")
+HDLR = logging.handlers.TimedRotatingFileHandler("log/dert.log",
+                                                 when="midnight", backupCount=60)
+STREAM_HDLR = logging.StreamHandler()
+FORMATTER = logging.Formatter("%(asctime)s %(filename)s [%(levelname)s] %(message)s")
+HDLR.setFormatter(FORMATTER)
+STREAM_HDLR.setFormatter(FORMATTER)
+PYTHON_LOGGER.addHandler(HDLR)
+PYTHON_LOGGER.addHandler(STREAM_HDLR)
+PYTHON_LOGGER.setLevel(logging.DEBUG)
+
+# Absolute path to the folder location of this python file
+FOLDER_ABSOLUTE_PATH = os.path.normpath(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Dert(tf.keras.layers.Layer):
+
+    def __init__(self, backbone=tf.keras.applications.ResNet50(weights="imagenet", include_top=False),
+                 backbone_preprocess=tf.keras.applications.imagenet_utils.preprocess_input):
+        super().__init__(Dert, self)
+        self.backbone = backbone
+        self.backbone_preprocess = backbone_preprocess
+
+        self.conv = tf.keras.layers.Conv2D(2048, 1,)
+
+    def call(self, x, **kwargs):
+        pass
