3878f7c14c637ba7e926e87bd2afbab20fc09397
swan.blanc <swan.blanc@infomaniak.com>
2020-07-13 15:30:03 +0200

    dert

--- k_deep/nn/dert.py
+++ k_deep/nn/dert.py
@@ -51,8 +51,8 @@ class Dert(tf.keras.layers.Layer):
         # prediction heads, one extra class for predicting non-empty slots
         # note that in baseline DETR linear_bbox layer is 3-layer MLP
         # now the model will take as input arrays of shape (*, hidden_dim)
-        self.linear_class = tf.keras.layers.Dense(num_classes + 1, input_shape=(hidden_dim,))
-        self.linear_bbox = tf.keras.layers.Dense(4, input_shape=(hidden_dim,))
+        self.linear_class = tf.keras.layers.Dense(num_classes + 1, input_shape=(hidden_dim,), activation="softmax")
+        self.linear_bbox = tf.keras.layers.Dense(4, input_shape=(hidden_dim,), activation="sigmoid")
 
         # output positional encodings (object queries)
         self.query_pos = tf.Variable(tf.random.uniform((100, hidden_dim)))
@@ -68,15 +68,29 @@ class Dert(tf.keras.layers.Layer):
         # for col_embed & row_embed concatenation
         self.concatenate = tf.keras.layers.Concatenate()
 
-    def call(self, inputs, training=False):
+    def call(self, inputs, detection_ceil=0.7, training=False):
+        """
+
+        :param inputs: (batch size, image array, channel last)
+        :param detection_ceil: (float) proba detection ceil
+        :param training:
+        :return: (dict) Dert prediction output and Transformer attention weights. Dict format
+                {
+                    'decoder_layer<X>block1': attention_weights softmax result
+                        get from scaled_dot_product_attention of the block 1 decoder layer
+                    'decoder_layer<X>block2': attention_weights softmax result
+                        get from scaled_dot_product_attention of the block 2 decoder layer
+
+                    "pred_logits": prediction labels shape is (batch size, seq length, number of classes + 1),
+                    "pred_boxes": bouding box prediction shape is (batch size, seq length, 4)
+                }
+        """
         # Apply preprocess function
         if self.backbone_preprocess is not None:
             inputs = self.backbone_preprocess(inputs)
 
-        print("b call")
         # propagate inputs through backbone model and get output features
         x = self.backbone(inputs)
-        print("e call")
 
         # convert from 2048 to 256 feature planes for the transformer
         h = self.conv(x)
@@ -96,13 +110,30 @@ class Dert(tf.keras.layers.Layer):
 
         # Concat this to array to have (H, W, hidden_dim) array
         pos = tf.concat([tile_expand_col, tile_expand_row], -1)
-        flatten_pos = tf.reshape(pos, (H*W, self.hidden_dim))
+        flatten_pos = tf.reshape(pos, (H * W, self.hidden_dim))
         # Set position to shape 1, H*W, hidden_dim
         flatten_pos = tf.expand_dims(flatten_pos, 0)
 
         # shape: (batch, H*W, features). Need to be the same hase the query pos
-        flatten_features = tf.reshape(h, (batch, H*W, self.hidden_dim))
+        flatten_features = tf.reshape(h, (batch, H * W, self.hidden_dim))
 
         # Get attention sequence shape same has query pos (batch, 100, hidden_dim)
         h, attention_weights = self.transformer(flatten_pos + 0.1 * flatten_features, self.query_pos, training)
-        print(h.shape)
+
+        pred_logits = self.linear_class(h)
+        pred_boxes = self.linear_bbox(h)
+
+        # remove the last fake classe and keep only predictions with 0.7+ confidence
+        probas = pred_logits[:, :, :-1]
+
+        keep = tf.reduce_max(probas, -1) > detection_ceil
+        for img_index, proba_keep in enumerate(keep):
+            keep_bbox = tf.boolean_mask(pred_boxes[img_index], proba_keep)
+            # TODO rescale_bboxes
+        attention_weights.update({
+            "pred_logits": pred_logits,
+            "pred_boxes": pred_boxes,
+
+        })
+
+        return attention_weights
